5f325cdb476d385f70f8c4f2cdd2a3e7398b4b8a
swarup1987 <62509089+swarup1987@users.noreply.github.com>
2025-05-23 18:38:38 +0530

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-import numpy
-
-setup(
-    name="cycore",
-    ext_modules=cythonize("cycore.pyx"),
-    include_dirs=[numpy.get_include()],
-)
