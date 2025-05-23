bef85d9a87cd024ef30c2fef826f5edeb7f8d4d7
swarup1987 <62509089+swarup1987@users.noreply.github.com>
2025-05-23 18:37:48 +0530

    Delete project_paths.py

--- project_paths.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-# The root is the current working directory from which the script is run.
-PROJECT_ROOT = Path().resolve()
-
-def data_path(filename):
-    """Get the full path to a data file in the project root."""
-    return PROJECT_ROOT / filename
